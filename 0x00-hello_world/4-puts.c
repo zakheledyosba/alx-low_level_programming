9a8aadb5935bdfeecd667f7fdbe1fbe9427d7ded
zakheledyosba <zakheledyosiba94@gmail.com>
2023-06-17 13:26:06 -0700

    first coding of C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-int main(void) {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
-}
+
+
 /**
- * main - main block
+ * Main - Main block
  *
  * Return: 0
  */
+
+int main(void)
+
+{
+	puts("Programming is like building a multilingual puzzle");
+	
+	return 0;
+}
