449e2698e8489abb66633a88dcfd8d4dd467ab8b
zakheledyosba <zakheledyosiba94@gmail.com>
2023-06-17 14:28:30 -0700

    first coding of C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n"with proper grammar, but the outcome is a piece of art")
+
+	Return: (0)
+}	
