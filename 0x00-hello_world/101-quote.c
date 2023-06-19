61de4480354e367e9efff85679c0ea272e41bb4a
zakheledyosba <zakheledyosiba94@gmail.com>
2023-06-19 14:11:06 -0700

    my first code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,16 @@
 
 
 /**
- * main - prints""prints exactly and that piece of art is useful" -
- *  Dora Korpar, 2015-10-19,"
+ * main - prints"prints exactly and that piece of art is useful" -Dora Korpar, 2015-10-19,"
+ *  followed by a new line, to the standard error.
  *  Return: 1
- *\
+ */
+ 
 
  int main(void)
 
  {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); 
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"),59); 
 
 	return (1);
 
