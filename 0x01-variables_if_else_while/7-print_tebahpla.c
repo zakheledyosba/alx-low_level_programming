3d49ad100437fee419c4733d16e00322099eb5d1
zakheledyosba <zakheledyosiba94@gmail.com>
2023-06-22 13:25:36 -0700

    processing c variables

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "the lowercase alphabet in reverse,"
+ * followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char letter;
+
+	for(letter = 'z';letter >= 'a'; leterr --)
+
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+
+}
