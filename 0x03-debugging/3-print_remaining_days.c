6c7f4d8eaed1b11d9494ab5805f8f1fa722d4600
zakheledyosba <zakheledyosiba94@gmail.com>
2023-06-24 07:39:02 -0700

    bebugging for C

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -33,7 +33,7 @@ void print_remaining_days(int month, int day, int year)
 
 	}
 
-		else
+	else
 
 	{
 
@@ -44,7 +44,6 @@ void print_remaining_days(int month, int day, int year)
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 
 		}
-
 
 		else
 
