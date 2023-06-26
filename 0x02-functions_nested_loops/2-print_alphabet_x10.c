138362c2163403ffc1d8f60c50f8dce8c753f507
zakheledyosba <zakheledyosiba94@gmail.com>
2023-06-26 11:24:05 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@
  * Return: 0
  */
 
-void print_alphabet_x10(void)
+void print_alphabet(void)
 
 {
 
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,7 +13,6 @@
 int print_sign(int n)
 
 {
-
 	if (n > 0 )
 
 	{
@@ -21,26 +20,6 @@ int print_sign(int n)
 		_putchar('+');
 
 		return (1);
-
-	}
-
-	else if (n == 0)
-
-	{
-
-		_putchar('0');
-
-		return (0);
-
-	}
-
-	else
-
-	{
-
-		_putchar('-');
-
-		return (-1);
 
 	}
 
