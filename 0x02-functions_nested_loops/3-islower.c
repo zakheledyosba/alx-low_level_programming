67bef756ac32c789994c4acbf146722b6700b338
zakheledyosba <zakheledyosiba94@gmail.com>
2023-06-26 10:36:15 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * main - Write a function that checks for lowercase character.
+ * @c: is the char to be checked
+ * Return: 0
+ *
+ */
+
+int _islower(int c)
+
+{
+
+	if(c >='a' && c <= 'z')
+
+		return (1);
+
+	else
+
+		return (0);
+
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * main - Write a function that checks for alphabetic character. 
+ * @c: is the char to be checked
+ * Return: 0
+ */
+
+int _isalpha(int c)
+
+{
+
+	if((c >= 'a' && c <= 'z') ||
+
+	  ((c >= 'A' && c <= 'Z'))
+
+	  	return (1);
+
+	else
+
+		return (0);
+
+}
