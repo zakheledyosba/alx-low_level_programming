c30bc2858af68cdbab007042919f9a16bb00e8c4
zakheledyosba <zakheledyosiba94@gmail.com>
2023-06-28 14:35:58 -0700

    C - More pointers, arrays and strings C

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,6 @@
 char *cap_string(char *str)
 
 {
-
 	int index = 0;
 
 	while (str[index])
@@ -23,36 +22,36 @@ char *cap_string(char *str)
 			index++;
 
 		if (str[index - 1] == ' ' ||
+		
+		    str[index - 1] == '\t' ||
 
-	    			str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
 
-	    			str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
 
-	    			str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
 
-	    			str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
 
-	    			str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
 
-	    			str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
 
-	    			str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
 
-	    			str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
 
-	    			str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
 
-	    			str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
 
-	    			str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
 
-	    			str[index - 1] == '}' ||
+		    index == 0)
 
-	    			index == 0)
+			str[index] -= 32;
 
-				str[index] -= 32;
-
-				index++;
+		index++;
 
 	}
 
