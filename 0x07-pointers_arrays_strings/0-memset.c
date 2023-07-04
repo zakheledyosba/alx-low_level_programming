ad1c2365f0991fb2faa3400daa6409b8858b91b8
zakheledyosba <zakheledyosiba94@gmail.com>
2023-07-04 09:41:34 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,34 +1,30 @@
 #include "main.h"
 
 
-/*
- * main - "Write a function that fills memory with a constant byte.
- * @s: pointer to start of memory area
- * @b: constant byte to fill with
- * @n: num bytes to fill in"
- * 
- * Return: pointer to beginning of memory area s
+/**
+ * _memset - fills memory with a constant byte
  *
+ * @s: pointer to memory area to fill
+ * @n: the number of bytes to fill
+ * @b: constant byte
+ *
+ * Return: a pointer to the memory area @s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 
 {
 
-	unsigned int i = 0;
+	unsigned int i;
 
-	char *start = s;
-
-	while (i < n)
+	for (i = 0; i < n; i++)
 
 	{
 
-		*s = b;
-
-		i++;
+		s[i] = b;
 
 	}
 
-	return (start);
+	return (s);
 
 }
