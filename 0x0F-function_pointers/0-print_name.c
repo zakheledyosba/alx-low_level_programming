51c5e8492aa09269d5cfab91fbae9a3809dc3bba
zakheledyosba <zakheledyosiba94@gmail.com>
2023-07-20 03:46:08 -0700

    Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,16 +3,18 @@
 
 
 /**
- * print_name - " print name using pointer to function
- * @name:string to add
- * @f:pointer to function"
- *
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
  * Return: nothing
- */
+ **/
 
-void print_name(char *name, void(*f)(char*))
+void print_name(char *name, void (*f)(char *))
+
 {
 	if (name == NULL || f == NULL)
 		return;
+
 	f(name);
+
 }
