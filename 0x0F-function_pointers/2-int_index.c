222549f9a91592fc639f15ae1dd83bc98cb57f66
zakheledyosba <zakheledyosiba94@gmail.com>
2023-07-20 03:36:51 -0700

    Function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 
 /**
@@ -11,12 +10,14 @@
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
-{
 
+{
 	int i;
+
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
-	for (i = o; i < size; i++)
+
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
