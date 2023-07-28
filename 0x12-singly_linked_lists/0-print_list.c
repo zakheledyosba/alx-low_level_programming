4f931875fc23c9e11d81d7fa1f3b996a021eb8fe
zakheledyosba <zakheledyosiba94@gmail.com>
2023-07-28 00:59:20 -0700

    Printlist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 
 
 /**
- * print_list -" function that prints all the elements of a list_t list.
+ * print_list - "function that prints all the elements of a list_t list.
  * @h: This take the value of the single list"
  *
  * Return: Return: the number of nodes
