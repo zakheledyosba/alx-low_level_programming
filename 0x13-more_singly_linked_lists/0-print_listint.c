e09ba744549b73f8b4940ff2045b3f4d8e62555c
zakheledyosba <zakheledyosiba94@gmail.com>
2023-07-31 13:58:04 -0700

    more linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,16 +12,16 @@
  */
 
 size_t print_listint(const listint_t *h)
-
 {
-	size_t num = 0;
+	size_t x = 0;
 
 	while (h)
+
 	{
 		printf("%d\n", h->n);
-		num++;
+		x++;
 		h = h->next;
 	}
 
-	return (num);
+	return (x);
 }
