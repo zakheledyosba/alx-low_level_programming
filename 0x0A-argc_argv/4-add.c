4d117cad3b32111a726f96cf705f160644ebb63b
zakheledyosba <zakheledyosiba94@gmail.com>
2023-08-02 06:36:45 -0700

    updated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,64 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 
 /**
- * main - "Write a program that adds positive numbers.
- * @argc: number of arguments
- * @argv: array of arguments"
- * Return: always 0
+ * check_num - "check - string there are digit
+ * @str: array str"
+ * Return: Always 0 (Success)
+ */
+
+int check_num(char *str)
+
+{
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - "Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments"
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 
 {
 
-	int i, suma = 0, res = 0;
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-	char c[] = "Error", *find_letter;
-
-	if (argc > 1)
-
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
 	{
-
-		for (i = 1; i < argc; i++)
+		if (check_num(argv[count]))
 
 		{
-
-			find_letter = argv[i];
-
-			while (*find_letter != 0)
-
-			{
-
-				if (*find_letter < 47 || *find_letter > 57)
-
-				{
-
-					printf("%s\n", c);
-
-					return (1);
-
-				}
-
-				find_letter++;
-
-			}
-
-			res = atoi(argv[i]);
-
-			suma += res;
-
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
 		}
 
-		printf("%d\n", suma);
+		/*Condition if one of the number contains symbols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
 
+		count++;
 	}
 
-	else
-
-		printf("%d\n", 0);
+	printf("%d\n", sum); /*print sum*/
 
 	return (0);
-
 }
