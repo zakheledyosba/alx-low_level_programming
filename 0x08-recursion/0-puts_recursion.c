9c011b4ad630139d0e59524c9e107101d9edc8df
zakheledyosba <zakheledyosiba94@gmail.com>
2023-09-01 09:36:54 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 
 /**
- * _puts_recursion - "function puts()
+ * _puts_recursion - " prints a string followed by a new line
  * @s:input"
  * Return: 0
  */
@@ -12,8 +12,8 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
-	else
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
