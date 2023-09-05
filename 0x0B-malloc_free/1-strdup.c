d92a361be33b2af8b9dcb2ddfc2b89a926831437
zakheledyosba <zakheledyosiba94@gmail.com>
2023-09-05 14:30:53 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,22 +12,22 @@
 char *_strdup(char *str)
 
 {
-        char *x;
-        int i = 0 , r = 0;
+	char *x;
+	int i = 0 , r = 0;
 
-        if (str == NULL)
-                return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-        while (str[i] != '\0')
-                i++;
+	while (str[i] != '\0')
+		i++;
 
-        x = malloc(sizeof(char) * (i + 1));
+	x = malloc(sizeof(char) * (i + 1));
 
-        if (x == NULL)
-                return (NULL);
+	if (x == NULL)
+		return (NULL);
 
-        for (r = 0; str[r]; r++)
-                x[r] = str[r];
+	for (r = 0; str[r]; r++)
+	x[r] = str[r];
 
-        return (x);
+	return (x);
 }
